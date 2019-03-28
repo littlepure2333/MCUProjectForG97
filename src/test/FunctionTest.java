c8c848917e50586d60fa893d4b9e24554179d21d
Peter Shen <minus190710@163.com>
2019-03-28 08:27:18 +0800

    update JUnit dependencies 4 => 5

--- src/test/FunctionTest.java
+++ src/test/FunctionTest.java
@@ -4,18 +4,18 @@ import data.UserList;
 import data.User;
 import bin.UserManage;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 
-public class FunctionTest {
+class FunctionTest {
     /*
      * 将一个默认的用户信息列表保存到本地文件:user.xml
      * 再从文件中取出用户信息列表，并列出所有用户的验证信息的完整性
      */
     @Test
-    public void testSetUserData() {
+    void testSetUserData() {
         UserList list = new UserList();
         ArrayList<User> userList = new ArrayList<>();
         userList.add(new User(123456780,"first","aaa@qmul.ac.uk"));
@@ -32,7 +32,7 @@ public class FunctionTest {
      * 最后打印一次所有用户的qm号来验证用户信息是否添加
      */
     @Test
-    public void testRegistration() {
+    void testRegistration() {
         System.out.println(UserManage.registration(100000000,"aaa","ccc@qmul.ac.uk"));
         UserList userList = new UserList();
         for (User user: userList.getUserList()) {
