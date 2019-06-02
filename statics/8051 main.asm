7b07b844dc31e43254376314ea7234f97023de82
littlepure2333 <rz8217525@163.com>
2019-06-02 23:26:43 +0800

    incomplete 3

--- statics/8051 main.asm
+++ statics/8051 main.asm
@@ -1,30 +1,34 @@
-    LENGTH EQU 3    ; 数据包的长度
-    RDFA   EQU 40H  ; 接受数据块首地址 (Receive Data First Address)
-                    ; MCU接受的数据块放置在内部RAM区的40～4F单元中
-    SDFA   EQU 50H  ; 发送数据块首地址 (Send Data First Address)
-                    ; MCU发送的数据块放置在内部RAM区的50～5F单元中
-    KEYBOARDTEMP EQU 60H
-    
-    LED_SEND_INIT       EQU 0x10
-    LED_SEND_FLASH      EQU 0x11
-    KEYBOARD_INIT       EQU 0x12;To check the keyboard interface
-    TAKEORRETURN_INIT   EQU 0x13;To check the takeorreturn function interface
-    YES_INIT            EQU 0x14;To check the admit function interface
-    LCD_INIT            EQU 0x15
-    LCD_HELLO           EQU 0x16
-    LCD_CHOOSE_ACT      EQU 0x17
-    LCD_ID_NOT_EXIST    EQU 0x18
-    LCD_ID_INVALID      EQU 0x19
-    DATA_END            EQU 0x7F
-;   这里放别的数据类型
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                   1 定义常量                                      ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;........ 1.1 数据块相关常量.........................................
+    LENGTH          EQU 3   ; 数据包的长度
+    RDFA            EQU 40H ; 接受数据块首地址 (Receive Data First Address)
+                            ; MCU接受的数据块放置在内部RAM区的40～4F单元中
+    SDFA            EQU 50H ; 发送数据块首地址 (Send Data First Address)
+                            ; MCU发送的数据块放置在内部RAM区的50～5F单元中
+    KEYBOARDTEMP    EQU 60H ; 键盘输入缓存，放置在内部RAM区的60～5F单元中
+;........ 1.2 数据类型常量................................................
+    LED_SEND_INIT       EQU 0x10 ; To initialize LED
+    LED_SEND_FLASH      EQU 0x11 ; LED flash
+    KEYBOARD_INIT       EQU 0x12 ; To initialize keyboard input
+    LCD_INIT            EQU 0x15 ; To initialize LCD
+    LCD_HELLO           EQU 0x16 ; LCD display 'Input your id:'
+    LCD_CHOOSE_ACT      EQU 0x17 ; LCD display 'Take/Return? 1/0'
+    LCD_ID_NOT_EXIST    EQU 0x18 ; LCD display 'ID doesnot exist'
+    LCD_ID_INVALID      EQU 0x19 ; LCD display 'Invaild ID!'
+    DATA_END            EQU 0x7F 
+;   这里放别的数据类型
+;........ 1.3 LCD相关参数.................................................
     RS EQU P1.5
     RW EQU P1.6
     EN EQU P1.7
-    LCM_DATA EQU P0    ;lcd
+    LCM_DATA EQU P0    ;lcd data port
     
-
-;...............................................................
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                   中断设置                                        ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
     ORG 0000H
     LJMP MAIN
@@ -41,8 +45,11 @@
     ORG 002BH       ; Timer 2 中断程序入口    
     RETI            ; 中断返回
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                   主程序                                          ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     ORG 100h
-    
 MAIN:
     MOV SP, #1FH    ; 设置堆栈指针
     ACALL UART_INIT ; 初始化串行通信
@@ -75,57 +82,243 @@ CHECK7:
 CHECK8:
     CJNE A,#LCD_ID_INVALID,LAST; To check if it should go to the function YES_INIT
     ACALL LCD_ID_INVALID
+
 ;   这里可以插入别的子程序
+
 LAST:    SJMP LOOP       ; 循环
 
-;......................................................................
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                   工具函数                                        ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-UART_INIT: 
-    MOV TMOD, #20h   ; set timer 1 to auto-reload 
-    MOV TH1, #0FDh   ; set 9600 baud(@11.0592MHz) 
-    SETB TR1         ; start timer 1
-    MOV SCON, #50h   ; set 8-bit data and Mode 1
+;===================串行端口相关函数==================================
+
+UART_INIT: ; 
+    MOV TMOD, #20h      ; set timer 1 to auto-reload 
+    MOV TH1, #0FDh      ; set 9600 baud(@11.0592MHz) 
+    SETB TR1            ; start timer 1
+    MOV SCON, #50h      ; set 8-bit data and Mode 1
+    RET
+;...................................................................
+UART_READ: 
+    MOV R0, #RDFA       ; 设置MCU接收的数据块首地址
+    RECEIVE:  
+        JNB RI, $       ; 等待接收完一字节
+        MOV A, SBUF     ; 接受一字节数据
+        CLR RI
+        MOV @R0, A      ; 存到接收数据块区域
+        INC R0          ; 修改地址指针
+        CJNE A, #DATA_END, RECEIVE ; 判断数据块是否接收完毕
+    RET
+;...................................................................
+UART_SEND:
+    CLR TI              ; clear transmit intr flag
+    MOV SBUF, A         ; put byte in SBUF
+    JNB TI, $           ; wait till byte is sent
+    RET                 ; leave subroutine
+
+;=================蜂鸣提示音函数======================================
+
+BUZZ:
+	CPL P1.1            ; start buzz
+	ACALL DELAY1        ; wait for a while
+	CPL P1.1            ; stop buzz
+	RET
+
+;=================DELAY函数==========================================
+DELAY: 
+    MOV R4,#1
+    THERE: 
+        MOV R5,#255;2
+        HERE:
+            MOV R6,#255;2
+            DJNZ R6,$ ;2*255
+        DJNZ R5,HERE;2
+    DJNZ R4,THERE
     RET
 
-UART_READ: MOV R0, #RDFA ; 设置MCU接收的数据块首地址
-           ;MOV R1, #LENGTH ; 设置接收数据块长度
-RECEIVE:  JNB RI, $ ; 等待接收完一字节
-          MOV A, SBUF ; 接受一字节数据
-          CLR RI
-          MOV @R0, A ; 存到接收数据块区域
-          INC R0 ; 修改地址指针
-          CJNE A, #DATA_END, RECEIVE ; 判断数据块是否接收完毕
-RET
+DELAY1: 
+    MOV R6, #45
+    ERE: 
+        MOV R5,#255;2
+        DJNZ R5,$
+    DJNZ R6, ERE
+    RET
 
-UART_SEND:
-CLR TI ; clear transmit intr flag
-MOV SBUF, A ; put byte in SBUF
-JNB TI, $ ; wait till byte is sent
-RET ; leave subroutine
+DELAY2: 
+    MOV R4,#4
+    THERE2: 
+        MOV R5,#255;2
+        HERE2:
+            MOV R6,#255;2
+            DJNZ R6,$ ;2*255
+        DJNZ R5,HERE2;2
+    DJNZ R4,THERE2
+    RET
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                   功能子函数                                      ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;====================LED相关函数=====================================
 
 LED_SEND_INIT_FUNC:
-    ; todo
+    MOV R0, #RDFA   ;Save the address of data-1
+    MOV R1, #0FEh   ;col 11111110
+    MOV P1, R1      ;select col
+    MOV R5, #00h    ;This register is used to save the sum
+    INTEGRATE:
+        INC R0      ;Save the address of data
+        MOV A, @R0  ;move data to A
+        CJNE A,#7Fh, DISPLAYADD   ;If not the end, execute add and receive again
+    MOV A, R5       ;If it is the end, do not add and directly diplay the sum in the register
+    MOV P0, A       ;
     RET
+
+DISPLAYADD:
+    ADD A, R5
+    MOV R5, A
+    LJMP INTEGRATE
+
+;...................................................................
 
 LED_SEND_FLASH_FUNC:
-    ; todo
+    MOV R0, #RDFA   ;Save the address of data-1
+    MOV R1, #0FEh   ;col 11111110
+    MOV P1, R1      ;select col
+    INC R0  ;Save the address of data
+    MOV R3, #20   ;WAIT
+    MOV R7, P0
+    MOV A, @R0     ;move data to A
+    MOV R2, A
+    MOV A, R7
+FLASH:
+    SUBB A, R2
+    MOV P0, A      ;send LED bits
+    ACALL DELAY2
+    ADD A, R2    ;TURN OFF LED 
+    MOV P0, A      ;set LED to be off
+    ACALL DELAY2
+    DJNZ R3, FLASH
     RET
 
-DELAY: MOV R4,#1
-          THERE: MOV R5,#255;2
-               HERE:MOV R6,#255;2
-                    DJNZ R6,$ ;2*255
-               DJNZ R5,HERE;2
-           DJNZ R4,THERE
-       RET;1
+;================LCD相关函数============================================
 
-DELAY1: MOV R6, #45
-    ERE:   MOV R5,#255;2
-          DJNZ R5,$
-        DJNZ R6, ERE
-       RET;1
+; lcd strings
+TAB_HELLO:          DB 'Input your id:'
+TAB_CHOOSE_ACT:     DB 'Take/Return? 1/0'
+TAB_ID_NOT_EXIST:   DB 'ID doesnot exist'
+TAB_ID_INVALID:     DB 'Invaild ID!' 
 
-;.........................................................................Keyboard 9
+LCD_HELLO:
+    MOV DPTR, #TAB_HELLO
+    MOV R0, #14
+    ACALL W_STR
+    RET
+
+LCD_CHOOSE_ACT:
+    MOV DPTR, #TAB_CHOOSE_ACT
+    MOV R0, #16
+    ACALL W_STR
+    RET
+
+LCD_ID_NOT_EXIST: 
+    MOV DPTR, #TAB_ID_NOT_EXIST
+    MOV R0, #16
+    ACALL W_STR
+    RET
+
+LCD_ID_INVALID: 
+    MOV DPTR, #TAB_ID_INVALID
+    MOV R0, #11
+    ACALL W_STR
+    RET
+
+LCD_INIT:
+    MOV A,#00111000B		;总线8，行数2，小字
+    ACALL W_INST
+    MOV A,#00001000B		;关闭显示，光标和闪烁
+    ACALL W_INST
+    ACALL LCD_CLR            ;clear
+    MOV A,#00000110B		;mode-输入后光标R移，屏幕不动
+    ACALL W_INST
+    MOV A,#00001111B		;开启显示，开启光标和闪烁
+    ACALL W_INST
+    RET
+
+LAY:                ;读忙程序
+    PUSH 7
+    MOV LCM_DATA,#0FFH
+    MOV R7,#255
+    CLR RS
+    SETB RW
+    SETB EN
+    POLLBF:
+    JNB LCM_DATA.7, POLLOK
+    DJNZ R7,POLLBF
+    POLLOK:
+    CLR EN
+    POP 7
+    RET
+
+
+W_INST:				;input lcd instruction
+    ACALL LAY
+    CLR RS
+    CLR RW
+    SETB EN
+    MOV LCM_DATA,A
+    CLR EN
+    RET
+
+W_DATA:				;input single character
+    ACALL LAY
+    SETB RS
+    CLR RW
+    SETB EN
+    MOV LCM_DATA, A
+    CLR EN
+    RET
+
+RM_DATA:            ;退位
+    MOV A, #00010000B   ;退格
+    ACALL W_INST
+    MOV A, #00100000B   ;empty character
+    ACALL W_DATA
+    MOV A, #00010000B   ;退格
+    ACALL W_INST
+    RET
+
+W_STR:				    ;input string
+    PUSH ACC
+    ACALL LCD_CLR
+    MOV A,#10000000B		;设置起始读写地址（第一行）
+    ACALL W_INST
+    CLR A
+    LOOP1:
+    MOVC A,@A+DPTR
+    ACALL W_DATA
+    INC DPTR
+    CLR A
+    DJNZ R0, LOOP1
+    MOV A,#11000000B        ;设置起始读写地址（第二行）
+    ACALL W_INST
+    POP ACC
+    RET
+
+LCD_CLR:				;clear the screen
+    MOV A,#01H
+    ACALL W_INST
+    RET
+
+
+
+
+
+;===========键盘相关函数========================================
+;.....................................Keyboard 9
+
+
 ;Assume the number delievered by JAVA is stored into 30h
 
 ;For the function initialization
@@ -204,36 +397,6 @@ KEYBOARDMAIN: ACALL CHECKBUTTON;To check the button
       KEYBOARDENDLOGIN: SJMP KEYBOARDMAIN
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;FUNCTION to choose take:33h return:34h
-TAKEORRETURN: ACALL CHECKBUTTON
-              ACALL DELAY
-              
-              CHECKTAKE: CJNE A,#0x01,CHECKRETURN;If press take, that is 1
-                         MOV A,#33h
-                         ACALL UART_SEND;Transmit the head
-                         ACALL DELAY1
-                         MOV A,#7Fh
-                         ACALL UART_SEND;Transmit the tail
-                         RET
-              
-              CHECKRETURN: CJNE A,#0x02,TAKEORRETURN;If press return, that is 1
-                           MOV A,#34h
-                           ACALL UART_SEND;Transmit the head
-                           ACALL DELAY1
-                           MOV A,#7Fh
-                           ACALL UART_SEND;Transmit the tail
-                           RET
-              
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
-CHOOSEYES: ACALL CHECKBUTTON
-           ACALL DELAY
-           CJNE A,#0x0B,CHOOSEYES;If press confirm, that is 1
-           MOV A,#31h
-           ACALL UART_SEND;Transmit the head
-           MOV A,#7Fh
-           ACALL UART_SEND;Transmit the tail
-           RET
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;     
 CHECKBUTTON: MOV DPTR,#KEYTABLE;Load the tabel
              ACALL KEY
@@ -293,117 +456,6 @@ DB      0x00;Not in use
 DB      0x00;Not in use
 DB      0x00;Not in use
 DB      0x00;Not in use
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;   
-;LCD MODULE
-
-; lcd strings
-
-TAB_HELLO: DB 'Input your id:'
-TAB_CHOOSE_ACT: DB 'Take/Return? 1/0'
-TAB_ID_NOT_EXIST: DB 'ID doesnot exist'
-TAB_ID_INVALID: DB 'Invaild ID!' 
-
-LCD_HELLO:
-MOV DPTR, #TAB_HELLO
-MOV R0, #14
-ACALL W_STR
-RET
-
-LCD_CHOOSE_ACT:
-MOV DPTR, #TAB_CHOOSE_ACT
-MOV R0, #16
-ACALL W_STR
-RET
-
-LCD_ID_NOT_EXIST: 
-MOV DPTR, #TAB_ID_NOT_EXIST
-MOV R0, #16
-ACALL W_STR
-RET
-
-LCD_ID_INVALID: 
-MOV DPTR, #TAB_ID_INVALID
-MOV R0, #11
-ACALL W_STR
-RET
-
-LCD_INIT:
-MOV A,#00111000B		;总线8，行数2，小字
-ACALL W_INST
-MOV A,#00001000B		;关闭显示，光标和闪烁
-ACALL W_INST
-ACALL LCD_CLR            ;clear
-MOV A,#00000110B		;mode-输入后光标R移，屏幕不动
-ACALL W_INST
-MOV A,#00001111B		;开启显示，开启光标和闪烁
-ACALL W_INST
-RET
-
-LAY:                ;读忙程序
-PUSH 7
-MOV LCM_DATA,#0FFH
-MOV R7,#255
-CLR RS
-SETB RW
-SETB EN
-POLLBF:
-JNB LCM_DATA.7, POLLOK
-DJNZ R7,POLLBF
-POLLOK:
-CLR EN
-POP 7
-RET
-
-
-W_INST:				;input lcd instruction
-ACALL LAY
-CLR RS
-CLR RW
-SETB EN
-MOV LCM_DATA,A
-CLR EN
-RET
-
-W_DATA:				;input single character
-ACALL LAY
-SETB RS
-CLR RW
-SETB EN
-MOV LCM_DATA, A
-CLR EN
-RET
-
-RM_DATA:            ;退位
-MOV A, #00010000B   ;退格
-ACALL W_INST
-MOV A, #00100000B   ;empty character
-ACALL W_DATA
-MOV A, #00010000B   ;退格
-ACALL W_INST
-RET
-
-W_STR:				    ;input string
-PUSH ACC
-ACALL LCD_CLR
-MOV A,#10000000B		;设置起始读写地址（第一行）
-ACALL W_INST
-CLR A
-LOOP1:
-MOVC A,@A+DPTR
-ACALL W_DATA
-INC DPTR
-CLR A
-DJNZ R0, LOOP1
-MOV A,#11000000B        ;设置起始读写地址（第二行）
-ACALL W_INST
-POP ACC
-RET
-
-LCD_CLR:				;clear the screen
-MOV A,#01H
-ACALL W_INST
-RET
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;  
 
